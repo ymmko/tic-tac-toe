2c40c094f5b69e5060ad10abc5c9bd0e83a5099f
YMMKO <ymmko@users.noreply.github.com>
2023-05-05 19:30:51 -0400

    style updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ export default function Square() {
   return (
     <>
       <div className="board-row">
-       <button className="square">1</button>
+        <button className="square">1</button>
         <button className="square">2</button>
         <button className="square">3</button>
       </div>
